b552424c49236ed98c356341321f5351fe8dec25
Suraj Kanavehalli <SurajKanavehalli@air-watch.com>
2015-10-24 19:50:59 +0530

    Added a project BingMapsLocationDataParser.

---
+++ UberFareEstimate/BingLocationDataParser/LocationDataParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BingMapsLocationDataParser
+{
+    public class LocationDataParser
+    {
+        public static LocationData Parse (string jsonString)
+        {
+            return JsonConvert.DeserializeObject<LocationData>(jsonString);
+        }
+    }
+}
